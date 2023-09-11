0af68de2afa61f63c87895adeec59d36344db375
deeply <gabriel.akyol@icloud.com>
2023-09-11 15:27:08 +0200

    init

--- src/Account.java
+++ src/Account.java
@@ -1,11 +1,11 @@
 public class Account {
-    private final String accountNumber;
-    private String BranchCode;
+    private final Integer accountNumber;
+    private final String BranchCode;
     private double overdraftLimit;
     private double balance;
     private boolean isActive;
 
-    public Account (String accountNumber, String BranchCode, double overdraftLimit, double balance, boolean isActive) {
+    public Account (Integer accountNumber, String BranchCode, double overdraftLimit) {
         this.accountNumber = accountNumber;
         this.BranchCode = BranchCode;
         this.overdraftLimit = overdraftLimit;
@@ -13,6 +13,36 @@ public class Account {
         this.isActive = true;
     }
 
+    // get
+    public double getBalance() {
+        return balance;
+    }
 
+    // account number
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
+
+    // branch code
+    public String getBranchCode() {
+        return BranchCode;
+    }
+
+    // overdraft limit
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+
+    // is active
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+
+    // set
+    // set overdraft limit
+    public void setOverdraftLimit(double overdraftLimit) {
+        this.overdraftLimit = overdraftLimit;
+    }
 
 }
